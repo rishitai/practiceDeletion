ea4b9b4814d1d65ced27180dcd41f9cb25855a22
manoj.kumal <manoj.kumal@tai.com.np>
2024-05-31 17:48:08 +0545

    created vueflow project to practice deletion of node

---
+++ src/components/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import VueFlowMain from "./VueFlowMain.vue";
+</script>
+
+<template>
+  <VueFlowMain />
+</template>
---
+++ src/components/VueFlowMain.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref } from "vue";
+import { MarkerType, VueFlow } from "@vue-flow/core";
+
+const nodes = ref([
+  {
+    id: "start",
+    type: "input",
+    deletable: false,
+    position: { x: 300, y: 0 },
+    label: "Start",
+  },
+  {
+    id: "initiator",
+    label: "initiator",
+    type: "default",
+    position: { x: 300, y: 100 },
+    draggable: false,
+    deletable: false,
+  },
+  {
+    id: "end",
+    type: "output",
+    label: "Stop",
+    position: { x: 300, y: 400 },
+    deletable: false,
+  },
+]);
+
+const edges = ref([
+  {
+    id: "first-edge",
+    label: "first-edge",
+    source: "start",
+    target: "initiator",
+    type: "straight",
+    style: { strokeWidth: 4 },
+    markerEnd: MarkerType.ArrowClosed,
+    selectable: false,
+  },
+  {
+    id: `end-edge`,
+    label: "end-edge",
+    type: "straight",
+    source: `initiator`,
+    target: "end",
+    markerEnd: MarkerType.ArrowClosed,
+    selectable: false,
+  },
+]);
+</script>
+
+<template>
+  <div class="vueflow-container">
+    <VueFlow :nodes="nodes" :edges="edges" />
+  </div>
+</template>
